2e07cbaa9ebb3d4f5927026d92684d23da4a12b0
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-12-10 17:05:54 +0100

    not possible to have usePathname server side

--- jeannedarc/app/plus/reglement-interieur/page.tsx
+++ jeannedarc/app/plus/reglement-interieur/page.tsx
@@ -1,13 +1,9 @@
-import { usePathname } from "next/navigation";
-
-
 export default function Page() {
-	const pathname = usePathname();
-  return (
-    <>
-      <main >
-       <h1>Page: {pathname}</h1>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main>
+                <h1> Page: page name</h1>
+            </main>
+        </>
+    );
 }
