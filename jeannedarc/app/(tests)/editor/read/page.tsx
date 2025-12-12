ecbe9ff1f32620cf16e25f6a5a2742475d25dad0
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2025-12-12 14:09:59 +0100

    definitions, including some corrections since tests

--- jeannedarc/app/(tests)/editor/read/page.tsx
+++ jeannedarc/app/(tests)/editor/read/page.tsx
@@ -1,9 +1,9 @@
-import { ContenuTipTap, InterfacePage } from '@/lib/definitions';
+import { ContenuTipTap, PageInterface } from '@/lib/definitions';
 import { getTextSectionByUrl } from '../../../../lib/contentCrud';
 import {DisplayedText} from "@/components/DisplayedText/DisplayedText";
 
 export default async function Page() {
- const page:InterfacePage|undefined = await getTextSectionByUrl ("editor/read")
+ const page:PageInterface|undefined = await getTextSectionByUrl ("editor/read")
  const contenu:ContenuTipTap = page!.content
 const check = JSON.stringify(contenu, null, 2)
     return (
