b690e2317b738f728dad78cd57f8978b27677710
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2026-01-05 15:13:56 +0100

    create virtualsection for pages

--- jeannedarc/app/[...slug]/page.tsx
+++ jeannedarc/app/[...slug]/page.tsx
@@ -1,37 +1,10 @@
 import { auth } from "@clerk/nextjs/server";
-import { SectionImageTexteServer } from "@/components/Sections/SectionImageTexte/SectionImageTexteServer";
-import { SectionTexteServer } from "@/components/Sections/SectionTexte/SectionTexteServer";
-import { SectionTitreServer } from "@/components/Sections/SectionTitre/SectionTitreServer";
 import { getPageByUrl } from "@/lib/queries/contentCrudPage";
 import { getAllSectionsByPageId } from "@/lib/queries/contentCrudSection";
-import { SectionInterface } from "@/lib/schemas";
-import { SectionTexteTexteServer } from "@/components/Sections/SectionTexteTexte/SectionTexteTexteServer";
 import { SectionSelector } from "@/components/Sections/SectionSelector/SectionSelector";
+import { VirtualSection } from "@/components/Sections/VirtualSection/VirtualSection";
 
-interface VirtualSectionProps {
-    section: SectionInterface;
-    isAuth: boolean;
-}
 
-function VirtualSection({ section, isAuth }: VirtualSectionProps) {
-    switch (section.type) {
-        case "Titre":
-            return <SectionTitreServer isAuth={isAuth} section={section} />;
-        case "Texte":
-            return <SectionTexteServer isAuth={isAuth} section={section} />;
-        case "TexteTexte":
-            return (
-                <SectionTexteTexteServer isAuth={isAuth} section={section} />
-            );
-        case "ImageTexte":
-            return (
-                <SectionImageTexteServer isAuth={isAuth} section={section} />
-            );
-        default:
-            console.warn(`Type de section inconnu: ${section.type}`);
-            return null;
-    }
-}
 
 interface PageProps {
     params: Promise<{
