58310db0b8278056ae14499bbadcdba05670572a
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2026-01-05 15:14:27 +0100

    header without edit

---
+++ jeannedarc/components/Header/HeaderServer.tsx
@@ -0,0 +1,54 @@
+"use server";
+import Link from "next/link";
+import styles from "./Header.module.css";
+import Image from "next/image";
+import logo from "../../assets/images/ECOLE_JDA_LOGO_BLANC-01-temp.png";
+//import { SignOutButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { LogoutButton } from "../Buttons/LogoutButton/LogoutButton";
+import { HeaderClient } from "./HeaderClient";
+import { getPageByUrl } from "@/lib/queries/contentCrudPage";
+import { getAllSectionsByPageId } from "@/lib/queries/contentCrudSection";
+import { ContenuHeaderBtnInterface } from "@/lib/schemas";
+import { getAllContenuHeaderBtnsBySectionId } from "@/lib/queries/contentCrudContenu";
+
+ export type SectionWithBtn= ContenuHeaderBtnInterface[]
+
+export async function HeaderServer() {
+	const page = await getPageByUrl ('header')
+	if (!page) {
+			return <p>Erreur au chargement de la page</p>;
+		}
+	const sections = await getAllSectionsByPageId(page?.id_page);
+	if (!sections) {
+			return <p>Erreur au chargement des sections de la page</p>;
+		}
+
+
+	const sectionsWithBtn:SectionWithBtn[]=[]
+	
+	for (let i=0; i<sections.length; i++){
+		const currentSection = await getAllContenuHeaderBtnsBySectionId(sections[i].id_section)
+		if (currentSection){sectionsWithBtn.push(currentSection)}
+	}
+
+    const { userId } = await auth();
+    const isAuth = !!userId;
+
+    return (
+        <header ><div className={styles.header}>
+            <Link href="/">
+                <Image
+                    src={logo}
+                    alt="Le logo de l'école Jeanne d'Arc"
+                    className={styles.logo}
+                />
+            </Link>
+            <HeaderClient isAuth={isAuth} sections={sectionsWithBtn}/>
+            <p>(+33)5 56 08 52 16</p></div>
+            {isAuth && (
+                <LogoutButton/>
+            )}
+        </header>
+    );
+}
--- jeannedarc/components/Icons/Icons.tsx
+++ jeannedarc/components/Icons/Icons.tsx
@@ -158,3 +158,23 @@ export function LogoutIcon({ className, style }: IconProps) {
         </svg>
     );
 }
+
+export function ChevronDown({ className, style }: IconProps) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={className}
+            style={style}
+        >
+            <path d="m6 9 6 6 6-6" />
+        </svg>
+    );
+}
