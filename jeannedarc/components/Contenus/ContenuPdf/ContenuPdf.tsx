a5de1407a3a0da7ba8eaab91641d041fd97f08c3
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2026-01-08 12:06:05 +0100

    end of pdf content with download button

--- jeannedarc/components/Contenus/ContenuPdf/ContenuPdf.tsx
+++ jeannedarc/components/Contenus/ContenuPdf/ContenuPdf.tsx
@@ -9,12 +9,20 @@ interface ContenuPdfProps {
 export function ContenuPdf({ contenu }: ContenuPdfProps) {
     return (
         <>
-            <iframe 
-				src={contenu.pdf_url}
-				className={styles.pdfContainer}
-				>
-			</iframe>
-			<p>{contenu.pdf_titre}</p>
+            <iframe
+                src={contenu.pdf_url}
+                className={styles.pdfContainer}
+            ></iframe>
+            <div className={styles.downloadContainer}>
+                <p className={styles.pdfTitle}>{contenu.pdf_titre}</p>
+                <a
+                    href={contenu.pdf_url}
+                    download
+                    className={styles.downloadBtn}
+                >
+                    Télécharger
+                </a>
+            </div>
         </>
     );
 }
