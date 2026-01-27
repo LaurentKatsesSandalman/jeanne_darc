dbf3e659f6e5fa213d17204ac2ba9490043a34dd
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2026-01-27 15:58:44 +0100

    skip link in layout and pages

--- jeannedarc/app/plus/page.tsx
+++ jeannedarc/app/plus/page.tsx
@@ -1,33 +1,36 @@
 import { getAllPlusPages } from "@/lib/queries/contentCrudPage";
 import { Metadata } from "next";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import { ContenuPlus } from "@/components/Contenus/ContenuPlus/ContenuPlusServer";
-
 
 export const revalidate = 86400;
 
 export async function generateMetadata(): Promise<Metadata> {
-    
     return {
         title: "Plus",
-        description: "École Jeanne d'Arc - Le Bouscat - ETABLISSEMENT CATHOLIQUE sous TUTELLE DIOCESAINE sous CONTRAT AVEC L'ETAT",
+        description:
+            "École Jeanne d'Arc - Le Bouscat - ETABLISSEMENT CATHOLIQUE sous TUTELLE DIOCESAINE sous CONTRAT AVEC L'ETAT",
     };
 }
 
 export default async function Page() {
+    const pages = await getAllPlusPages();
+    if (!pages) {
+        return <p>Erreur au chargement de la page ou page inexistante</p>;
+    }
 
-const pages = await getAllPlusPages();
-if (!pages){
-	return <p>Erreur au chargement de la page ou page inexistante</p>;
-}
-
-return (<main><div  className={styles.plusPageContainer}>
-	<h1 className={styles.h1}>Liste des pages de la section &quot;Plus&quot;</h1>
-	<div className={styles.resultGrid}>
-{pages.map((page)=> (
-	<ContenuPlus  key={page.id_page} page={page}/>
-))}
-</div>
-</div>
-</main>)
+    return (
+        <main id="main-content">
+            <div className={styles.plusPageContainer}>
+                <h1 className={styles.h1}>
+                    Liste des pages de la section &quot;Plus&quot;
+                </h1>
+                <div className={styles.resultGrid}>
+                    {pages.map((page) => (
+                        <ContenuPlus key={page.id_page} page={page} />
+                    ))}
+                </div>
+            </div>
+        </main>
+    );
 }
--- jeannedarc/app/recherche/page.tsx
+++ jeannedarc/app/recherche/page.tsx
@@ -1,6 +1,6 @@
 import { searchIndexAction } from "@/lib/actions/actionsIndex";
 import { SearchIndexResult } from "@/lib/schemas";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import { ResultatRecherche } from "@/components/ResultatRecherche/ResultatRecherche";
 
 // export const dynamic = 'force-dynamic';
@@ -12,24 +12,31 @@ export default async function Page({
     searchParams: Promise<{ q?: string }>;
 }) {
     const params = await searchParams;
-	const recherche = await params.q || "";
+    const recherche = (await params.q) || "";
     const resultats: SearchIndexResult | null = recherche
         ? await searchIndexAction(recherche)
         : null;
 
-	const noResult = (!resultats || (resultats.success&&resultats.data.length === 0) || !resultats.success)
+    const noResult =
+        !resultats ||
+        (resultats.success && resultats.data.length === 0) ||
+        !resultats.success;
 
     return (
-        <main>
-			<div className={styles.searchPageContainer}>
-            <h1 className={styles.h1}>Résultats de la recherche</h1>
-            <div className={styles.resultGrid}>
-				{noResult&&<h2>Pas de résultat</h2>}
-                {resultats?.success &&
-                    resultats.data.map((resultat) => (
-                        <ResultatRecherche resultat={resultat} key={resultat.id_page_fk} />
-                    ))}
-            </div></div>
+        <main id="main-content">
+            <div className={styles.searchPageContainer}>
+                <h1 className={styles.h1}>Résultats de la recherche</h1>
+                <div className={styles.resultGrid}>
+                    {noResult && <h2>Pas de résultat</h2>}
+                    {resultats?.success &&
+                        resultats.data.map((resultat) => (
+                            <ResultatRecherche
+                                resultat={resultat}
+                                key={resultat.id_page_fk}
+                            />
+                        ))}
+                </div>
+            </div>
         </main>
     );
 }
