5e3fd920efe7fe5987b8d9eb73f679eeb6a25363
LaurentKatsesSandalman <laurent.durup@numericable.fr>
2026-01-28 17:21:29 +0100

    rel=noopener noreferrer and max-width instead of width in images

--- jeannedarc/components/Contenus/ContenuSoloBtn/ContenuSoloBtn.tsx
+++ jeannedarc/components/Contenus/ContenuSoloBtn/ContenuSoloBtn.tsx
@@ -15,6 +15,6 @@ const isLocal = contenu.lien_vers.startsWith("/")
 	return (<div className={url==="/"?styles.containerSoloHome:styles.containerSolo}>
 	
 {isLocal?(<Link href={contenu.lien_vers} className={styles.bouton} > {contenu.bouton}
-</Link>):(<a href={contenu.lien_vers} className={styles.bouton}>{contenu.bouton}</a>)}
+</Link>):(<a href={contenu.lien_vers} className={styles.bouton} rel="noopener noreferrer">{contenu.bouton}</a>)}
 	</div>)
 }
